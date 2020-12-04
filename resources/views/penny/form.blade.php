25f15362e0c9612f6e4655d31b50953955f6f663
Fabian <19959@ma-web.nl>
2020-12-04 19:20:35 +0100

    Create form.blade.php

---
+++ resources/views/penny/form.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+@section('page_title')
+    Penny Toevoegen
+@endsection
+@section('content')
+    <form action="" method="post">
+        <input type="text" name="" id="">
+        <input type="submit" value="Voeg toe">
+    </form>
+@endsection
