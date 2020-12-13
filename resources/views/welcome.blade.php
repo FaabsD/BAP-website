54f7f2968ed1a83c6c25a4f44b0f2b7f7689a182
Fabian <19959@ma-web.nl>
2020-12-13 21:51:22 +0100

    url vervangen voor route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,21 +17,21 @@
                 @endauth
             </div>
         @endif--}}
-    <main class="grid grid-cols-2 gap-4">
-        <section class="col-span-full text-center p-4">
+    <main class="grid grid-cols-2 gap-4 overflow-hidden">
+        <section class="col-span-full text-center py-4">
             <h2 class="text-2xl">Welkom wat wilt u bekijken?</h2>
         </section>
-        <section class="text-center grid auto-rows-auto gap-2 p-4">
+        <section class="text-center grid auto-rows-auto gap-2">
             <h3 class="text-xl font-semibold">Penny verzameling</h3>
-            <a href="{{url('pennies')}}" class="block">
+            <a href="{{route('pennies')}}" class="block">
                 <img src="{{ asset('images/penny.jpg') }}" alt="Pressed penny"
                      class="w-full h-full lg:w-3/4 lg:h-3/4  object-cover m-auto">
             </a>
         </section>
-        <section class="text-center grid auto-cols-auto gap-2 p-4">
+        <section class="text-center grid auto-cols-auto gap-2">
             <h3 class="text-xl font-semibold">Memodaille verzameling</h3>
 
-            <a href="{{ url('memodailles') }}" class="block">
+            <a href="{{ route('coins') }}" class="block">
                 <img src="{{ asset('images/Memodaille.jpg') }}" alt="Memodaille"
                      class="w-full h-full lg:w-3/4 lg:h-3/4 object-contain m-auto">
             </a>
