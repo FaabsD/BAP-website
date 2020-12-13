7404882df2c7f199f4b6ba210fac0bb784b55cbd
Fabian <19959@ma-web.nl>
2020-12-13 21:51:07 +0100

    bezig met navigatie

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,6 +13,19 @@
     <h1 class="text-black text-4xl">Pressed Penny verzameling</h1>
     <h3 class="text-xl">Familie Tuijn & Kleinkinderen</h3>
 </header>
+<nav class="container">
+    <ul class="grid grid-flow-col auto-cols-fr">
+        <li>
+            <a href="{{route('pennies')}}">Pennies</a>
+        </li>
+        <li>
+            <a href="{{route('coins')}}">Memodailles</a>
+        </li>
+        <li>
+            <a href="{{route('about')}}">Over ons</a>
+        </li>
+    </ul>
+</nav>
 @yield("content")
 
 </body>
