b7915dc46442d996783af9a4be7f2f20f364cc3b
Fabian <19959@ma-web.nl>
2020-12-13 21:18:47 +0100

    veranderd naar simpel introscherm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,40 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('master')
+@section('page_title')
+    Home
+@endsection
+@section('content')
+    {{--<div class="flex flex-row justify-end">
+        @if (Route::has('login'))
+            <div class="">
+                @auth
+                    <a href="{{ url('/home') }}" class="p-4">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="p-4">Login</a>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="p-4">Register</a>
+                    @endif
+                @endauth
             </div>
-        </div>
-    </body>
-</html>
+        @endif--}}
+    <main class="grid grid-cols-2 gap-4">
+        <section class="col-span-full text-center p-4">
+            <h2 class="text-2xl">Welkom wat wilt u bekijken?</h2>
+        </section>
+        <section class="text-center grid auto-rows-auto gap-2 p-4">
+            <h3 class="text-xl font-semibold">Penny verzameling</h3>
+            <a href="{{url('pennies')}}" class="block">
+                <img src="{{ asset('images/penny.jpg') }}" alt="Pressed penny"
+                     class="w-full h-full lg:w-3/4 lg:h-3/4  object-cover m-auto">
+            </a>
+        </section>
+        <section class="text-center grid auto-cols-auto gap-2 p-4">
+            <h3 class="text-xl font-semibold">Memodaille verzameling</h3>
+
+            <a href="{{ url('memodailles') }}" class="block">
+                <img src="{{ asset('images/Memodaille.jpg') }}" alt="Memodaille"
+                     class="w-full h-full lg:w-3/4 lg:h-3/4 object-contain m-auto">
+            </a>
+        </section>
+    </main>
+@endsection
