31eb23110d450a01ba9485d7124a60847046de00
Fabian <19959@ma-web.nl>
2020-12-13 22:25:27 +0100

    verder met nav

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,22 +8,32 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>@yield("page_title")</title>
 </head>
-<body>
+<body class="overflow-x-hidden">
 <header class="w-screen py-6 bg-blue-200 text-center">
     <h1 class="text-black text-4xl">Pressed Penny verzameling</h1>
     <h3 class="text-xl">Familie Tuijn & Kleinkinderen</h3>
 </header>
-<nav class="container">
-    <ul class="grid grid-flow-col auto-cols-fr">
+<nav class="container lg:w-3/4 lg:mx-auto text-center">
+    <ul class="grid grid-flow-col">
         <li>
-            <a href="{{route('pennies')}}">Pennies</a>
+            <a href="{{route('pennies')}}" class="p-4 block bg-indigo-200">Pennies</a>
         </li>
         <li>
-            <a href="{{route('coins')}}">Memodailles</a>
+            <a href="{{route('coins')}}" class="p-4 block">Memodailles</a>
         </li>
         <li>
-            <a href="{{route('about')}}">Over ons</a>
+            <a href="{{route('about')}}" class="p-4 block">Over ons</a>
         </li>
+        @guest
+            <li >
+                <a href="{{ route('login') }}" class="p-4 block">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+                <li >
+                    <a href="{{ route('register') }}" class="p-4 block">{{ __('Register') }}</a>
+                </li>
+            @endif
+        @endguest
     </ul>
 </nav>
 @yield("content")
