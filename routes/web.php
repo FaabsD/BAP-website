e2e54a3c7bb87240655a6810deae327f4e34bc17
Fabian <19959@ma-web.nl>
2020-12-16 23:32:37 +0100

    routes aangepast

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/over-ons', 'AboutController@aboutMe')->name('about');
 Route::get('/pennies', 'PennyController@index')->name('pennies');
 // alleen beschikbaar voor ingelogde beheerder
 Route::get('/pennies/voeg-toe', 'PennyController@create')->name('penny.add');
-Route::post('/pennies/verwerken', 'PennyController@handlePennyForm')->name('penny.add.handle');
+Route::post('/pennies/verwerken', 'PennyController@store')->name('penny.add.handle');
 Route::get('/pennies/aanpassen/{id}', 'PennyController@updatePenny')->name('penny.update');
 // haal de pennies op bij alfabet
 Route::get('pennies/alfabetisch/{alphabet}', 'PennyController@getByAlphabet')->name('pennies.alphabet');
@@ -31,8 +31,8 @@ Route::get('/pennies/plaats/{town}', 'PennyController@getByTown')->name('pennies
 // verzameling memodailles
 Route::get('/memodailles', 'CoinController@getCoins')->name('coins');
 // alleen beschikbaar voor ingelogde beheerder
-Route::get('/memodailles/voeg-toe', 'CoinController@addCoinForm')->name('coin.add');
-Route::post('/memodailles/verwerken', 'CoinController@handleCoinForm')->name('coin.add.handle');
+Route::get('/memodailles/voeg-toe', 'CoinController@create')->name('coin.add');
+Route::post('/memodailles/verwerken', 'CoinController@store')->name('coin.add.handle');
 Route::get('/memodailles/aanpassen/{id}', 'CoinController@updateCoin')->name('coin.update');
 // haal de memodailles op bij alfabet
 Route::get('memodailles/alfabetisch{alphabet}', 'CoinController@getByAlphabet')->name('coin.alphabet');
