33a2f67b577a0e106097fda983be2e4813b56776
Fabian <19959@ma-web.nl>
2021-01-23 23:58:40 +0100

    toon de gëuploade afbeelding of een placeholder als er geen afbeelding gëupload is

--- resources/views/penny/all-pennies.blade.php
+++ resources/views/penny/all-pennies.blade.php
@@ -6,7 +6,11 @@
     <div class="penny-container container-fluid my-3">
         @foreach($pennies as $penny)
             <div class="card">
-                <img class="card-img-top" src="" alt="">
+                @if(!$penny->Afbeelding)
+                    <img src="https://via.placeholder.com/500?text=Afbeelding+niet+beschikbaar" alt="" class="card-img-top">
+                @else
+                    <img class="card-img-top" src="{{Storage::url($penny->Afbeelding)}}" alt="">
+                @endif
                 <div class="card-body">
                     <h3 class="card-title">{{$penny->Plaats}}</h3>
                     <h4 class="card-subtitle text-muted mb-2">{{$penny->Serie}}</h4>
