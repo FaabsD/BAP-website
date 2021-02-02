c63040aabffe3712092b80fca80120b55b1456be
Fabian <19959@ma-web.nl>
2021-02-02 09:59:50 +0100

    onnodige extra login/register balk weg gehaald

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,20 +3,6 @@
     Home
 @endsection
 @section('content')
-    <div class="flex flex-row justify-end">
-        @if (Route::has('login'))
-            <div class="">
-                @auth
-                    <a href="{{ url('/home') }}" class="p-4">Home</a>
-                @else
-                    <a href="{{ route('login') }}" class="p-4">Login</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="p-4">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
     <main class="welcome">
         <section class="welcome__section1">
             <h2 class="section1__head">Welkom wat wilt u bekijken?</h2>
