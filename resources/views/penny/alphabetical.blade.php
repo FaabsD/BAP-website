30c762469916908cb338180d0c4c4f6d97d02683
Fabian <19959@ma-web.nl>
2021-02-02 14:00:53 +0100

    toon de pennies die op alfabet zijn opgehaald

--- resources/views/penny/alphabetical.blade.php
+++ resources/views/penny/alphabetical.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.master')
-
+@section('page_title')
+    Pressed pennies {{mb_convert_case($alphabet, MB_CASE_UPPER, "UTF-8")}}
+@endsection
 @section('content')
-    <h2>alle pennies waarvan de plaats begint met {{$alphabet}}</h2>
+    {{--<?php dd($penny);?>--}}
+    <div class="penny-container container-fluid my-3">
+        @foreach($pennies as $penny)
+            <div class="card mt-2 mb-2">
+                @if(!$penny->Afbeelding)
+                    <img src="https://via.placeholder.com/500?text=Afbeelding+niet+beschikbaar" alt="" class="card-img-top">
+                @else
+                    <img class="card-img-top" src="{{Storage::url($penny->Afbeelding)}}" alt="">
+                @endif
+                <div class="card-body">
+                    <h3 class="card-title">{{$penny->Plaats}}</h3>
+                    <h4 class="card-subtitle text-muted mb-2">{{$penny->Serie}}</h4>
+                    <p class="card-text">
+                        {{$penny->Omschrijving}}<br>
+                        {{$penny->Positie}}
+                    </p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    {{$pennies->onEachSide(1)->links()}}
 @endsection
